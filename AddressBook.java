30de61256e092667e299c5646f699812c3870ce3
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-24 00:13:08 +0530

    Address book is created

---
+++ AddressBook.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class AddressBook {
+    void contactDetail() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Name ");
+        String first_name = sc.next();
+        System.out.println("Enter the last Name ");
+        String last_name = sc.next();
+        System.out.println("Enter the Address ");
+        String address = sc.next();
+        System.out.println("Enter City ");
+        String city = sc.next();
+        System.out.println("Enter State ");
+        String state = sc.next();
+        System.out.println("Enter Zip Code  ");
+        int PinCode = sc.nextInt();
+        System.out.println("Enter your Mobile Number ");
+        String mobileNumber = sc.next();
+        System.out.println("Enter Email Id  ");
+        String email = sc.next();
+        Contact add = new Contact(first_name, last_name, address, city, state, PinCode, mobileNumber, email);
+
+    }
+}
---
+++ Contact.java
@@ -0,0 +1,87 @@
+package org.example;
+
+public class Contact {
+    private String first_name;
+    private String last_name;
+    private String address;
+    private String city;
+    private String state;
+    private int pinCode;
+    private String mobileNumber;
+    private String email;
+
+    public Contact(String first_name, String last_name, String address, String city, String state, int pinCode, String mobileNumber, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.pinCode = pinCode;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getpinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        pinCode = pinCode;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
