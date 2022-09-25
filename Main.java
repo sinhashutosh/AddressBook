ff07ef019298d39e1a1025d98e63583b7375c950
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-25 16:40:37 +0530

    adding new Contact

--- Main.java
+++ Main.java
@@ -3,7 +3,12 @@ package org.example;
 public class Main {
     public static  void main(String[] args) {
         System.out.println("Welcome to Address Book System");
-        AddressBook address = new AddressBook();
-        address.addContactDetail();
+        AddressBook address1 = new AddressBook();
+        AddressBook address2 = new AddressBook();
+        System.out.println("Enter First Contact Detail ...");
+        address1.addContactDetail();
+        System.out.println("*****************************************************");
+        System.out.println("Enter Second Contact Detail ");
+        address2.addContactDetail();
     }
 }
--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-Ability to create a Contacts in Address
-Book with first and last names, address,
-city, state, zip, phone number and
-email…
-- Program is written using IDE like IntelliJ
-- Every UC is in a separate Git Branch and then merged with main
-- Naming Convention, Indentation, etc Code Hygiene will be checked during
-Review
-- Git Check In Comments and Version History will be monitored
+Ability to add a new
+Contact to Address Book 
+- Use Console to add person details from
+AddressBookMain class
+- Use Object Oriented Concepts to manage
+relationship between AddressBook and Contact
+Person
